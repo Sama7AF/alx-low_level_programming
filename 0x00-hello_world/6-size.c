53bcba18365fb17c0cdb355bb4ea5304d68f1f00
Sama7AF <samahabdelfatah326@gmail.com>
2023-06-15 13:51:06 -0700

    f2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various types based on
- *        the computer it is compiled and run on..
+ * main - Entry point
  *
  * Return: Always 0.
  */
