c1edc056eef36bf3825cea99a7b6d3be56c1b7aa
Sama7AF <samahabdelfatah326@gmail.com>
2023-07-07 16:57:18 -0700

    1st

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print programme name, followed by new line
+*@argc:integer
+*@argv:list
+* Return:0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
