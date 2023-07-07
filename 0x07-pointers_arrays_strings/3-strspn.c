651719ef72d372c20354d8a7ae38684fa9de5cd0
Sama7AF <samahabdelfatah326@gmail.com>
2023-07-07 14:57:10 -0700

    3rd

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn - new
+ * @accept: var
+ * @s: va
+ * Return: o
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
