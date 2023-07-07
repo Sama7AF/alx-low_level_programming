7b9d24312d6e84e7b7c1b65906b0b43ceb033ab9
Sama7AF <samahabdelfatah326@gmail.com>
2023-07-07 15:00:21 -0700

    4th

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strpbrk - m
+ * @accept: var
+ * @s: va
+ * Return: point
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
